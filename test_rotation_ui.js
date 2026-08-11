417eb971ac4add8c2b622ad6d0d162910470621c
kyho <rudvh1016@naver.com>
2026-08-12 01:04:56 +0900

    GAEO 꽃 브랜드 심볼 전역 교체

--- test_rotation_ui.js
+++ test_rotation_ui.js
@@ -7,7 +7,7 @@ const root = __dirname;
 const html = fs.readFileSync(path.join(root, 'index.html'), 'utf8');
 const serviceWorker = fs.readFileSync(path.join(root, 'sw.js'), 'utf8');
 
-assert.match(serviceWorker, /const CACHE = 'gaeo-shell-v7'/);
+assert.match(serviceWorker, /const CACHE = 'gaeo-shell-v8'/);
 assert.match(serviceWorker, /\(\?:html\|css\|js\|json\)\$/);
 
 assert.match(html, /data-nav-mode="rotation"[^>]*>순환매</);
